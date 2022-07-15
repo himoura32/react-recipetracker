e91434d8c5957bbe8a2c1c17b83fecd49a4709f8
himoura32 <shawn.ahearn@outlook.com>
2022-07-15 09:34:37 -0400

    coded routing, navbar comp, fetchData and CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Navbar from './components/Navbar';
+import Create from './pages/create/Create';
+import Recipe from './pages/recipe/Recipe';
+import Search from './pages/search/Search';
 
 function App() {
   return (
-    <div className="App">
-      Recipe Tracker
+    <div className='App'>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/create'>
+            <Create />
+          </Route>
+          <Route path='/search'>
+            <Search />
+          </Route>
+          <Route path='/recipes/:id'>
+            <Recipe />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
