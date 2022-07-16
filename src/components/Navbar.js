8db82c18cf1f17ffa704f7269342c2037b3dbace
himoura32 <shawn.ahearn@outlook.com>
2022-07-16 11:03:42 -0400

    coded context, reducers, color/dark mode selector

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import './Navbar.css';
 
-import { Link } from 'react-router-dom';
 import Searchbar from './Searchbar';
+import { Link } from 'react-router-dom';
+import { useTheme } from '../hooks/useTheme';
 
 export default function Navbar() {
+  const { color } = useTheme();
+
   return (
-    <div className='navbar'>
+    <div className='navbar' style={{ background: color }}>
       <nav>
         <Link to='/' className='brand'>
           <h1>Cooking Ninja</h1>
